4634a89ec1e9344c8ba792d2a0899ce87421f350
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 17:46:25 -0300

    component-scan

--- src/main/java/com/fundamentals/Controller/FundamentalsController.java
+++ src/main/java/com/fundamentals/Controller/FundamentalsController.java
@@ -1,4 +1,4 @@
-package com.fundamentals.spring_fundamentals_java;
+package com.fundamentals.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/fundamentals/spring_fundamentals_java/SpringFundamentalsJavaApplication.java
+++ src/main/java/com/fundamentals/spring_fundamentals_java/SpringFundamentalsJavaApplication.java
@@ -2,7 +2,9 @@ package com.fundamentals.spring_fundamentals_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "/com.fundamentals")
 @SpringBootApplication
 public class SpringFundamentalsJavaApplication {
 
