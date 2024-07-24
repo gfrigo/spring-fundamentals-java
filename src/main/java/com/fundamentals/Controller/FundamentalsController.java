3d322f5c64aae80b1433b7f5ba159d9e6013ce94
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 19:08:44 -0300

    header-param

--- src/main/java/com/fundamentals/Controller/FundamentalsController.java
+++ src/main/java/com/fundamentals/Controller/FundamentalsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,20 +18,26 @@ public class FundamentalsController {
   // Path param
   @GetMapping("/pathParam/{id}")
   public String pathParam(@PathVariable String id){
-    return "Path Param : " + id;
+    return "Path Param: " + id;
   }
 
   // /primeiraController/queryParam?id=19&nome=Gabriel
   @GetMapping("/queryParam")
   public String queryParam(@RequestParam Map<String, String> allParams){
-    return "Query Param : " + allParams.entrySet();
+    return "Query Param: " + allParams.entrySet();
   }
   
+  // Recuperar informações contidas no JSON da requisição
   @PostMapping("/bodyParam")
   public String bodyParam(@RequestBody Usuario usuario){
-    return "Body Param : " + usuario.username();
+    return "Body Param: " + usuario.username();
   }
 
   record Usuario(String username){}
+
+  @PostMapping("/headerParam")
+  public String headerParam(@RequestHeader Map<String, String> headers){
+    return "Header Params: " + headers.entrySet();
+  }
 
 }
