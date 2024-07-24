1b94e23126e6a515f9551521d3177204ac5040a7
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 17:28:13 -0300

    start-project-2

---
+++ src/main/java/com/fundamentals/spring_fundamentals_java/SpringFundamentalsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentals.spring_fundamentals_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentals/spring_fundamentals_java/SpringFundamentalsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentals.spring_fundamentals_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
