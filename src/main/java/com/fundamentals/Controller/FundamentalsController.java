ed37d41101731700f93aa0cc3eee4010a67f4205
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 18:47:37 -0300

    body-param

--- src/main/java/com/fundamentals/Controller/FundamentalsController.java
+++ src/main/java/com/fundamentals/Controller/FundamentalsController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +17,20 @@ public class FundamentalsController {
   // Path param
   @GetMapping("/pathParam/{id}")
   public String pathParam(@PathVariable String id){
-    return "Path Param:" + id;
+    return "Path Param : " + id;
   }
 
   // /primeiraController/queryParam?id=19&nome=Gabriel
   @GetMapping("/queryParam")
   public String queryParam(@RequestParam Map<String, String> allParams){
-    return "Query Param:" + allParams.entrySet();
+    return "Query Param : " + allParams.entrySet();
   }
   
+  @PostMapping("/bodyParam")
+  public String bodyParam(@RequestBody Usuario usuario){
+    return "Body Param : " + usuario.username();
+  }
+
+  record Usuario(String username){}
+
 }
