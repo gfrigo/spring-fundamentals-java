f8a96be2bfc91b568930aa4566b235a161c158a5
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 18:10:27 -0300

    query-param

--- src/main/java/com/fundamentals/Controller/FundamentalsController.java
+++ src/main/java/com/fundamentals/Controller/FundamentalsController.java
@@ -1,8 +1,11 @@
 package com.fundamentals.Controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +15,13 @@ public class FundamentalsController {
   // Path param
   @GetMapping("/pathParam/{id}")
   public String pathParam(@PathVariable String id){
-    return "Hello World!" + id;
+    return "Path Param:" + id;
   }
 
+  // /primeiraController/queryParam?id=19&nome=Gabriel
+  @GetMapping("/queryParam")
+  public String queryParam(@RequestParam Map<String, String> allParams){
+    return "Query Param:" + allParams.entrySet();
+  }
+  
 }
