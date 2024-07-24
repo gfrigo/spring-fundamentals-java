cde7514c2d5bd8d756cfa6ac95e51b34f77052a0
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 19:32:11 -0300

    response-entity

--- src/main/java/com/fundamentals/Controller/FundamentalsController.java
+++ src/main/java/com/fundamentals/Controller/FundamentalsController.java
@@ -2,6 +2,8 @@ package com.fundamentals.Controller;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/primeiraController")
 public class FundamentalsController {
+
+  record Usuario(String username){}
   
   // Path param
   @GetMapping("/pathParam/{id}")
@@ -33,11 +37,21 @@ public class FundamentalsController {
     return "Body Param: " + usuario.username();
   }
 
-  record Usuario(String username){}
-
   @PostMapping("/headerParam")
   public String headerParam(@RequestHeader Map<String, String> headers){
     return "Header Params: " + headers.entrySet();
+  }
+
+  // Retornar, mediante ação, uma resposta de status HTTP e informações no corpo
+  @GetMapping("/responseEntity/{id}")
+  public ResponseEntity<Object> responseEntity(@PathVariable int id){
+    Usuario usuario = new Usuario("Gabriel Frigo");
+
+    if(id > 5){
+      return ResponseEntity.status(HttpStatus.OK).body("Sucesso! " + usuario.username());
+    }else{
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro!");
+    }
   }
 
 }
